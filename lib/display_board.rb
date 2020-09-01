a97a7ff77a72cb2e409f5541b097659d43980e60
Emma Danica <ms.bee.indeed@gmail.com>
2020-09-01 10:32:36 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 def display_board(board)
-  board = [" "," "," "," "," "," "," "," "," "]
+  board = [" "," "," ","X"," "," "," "," "," "]
 puts "   |   |   "
 puts "-----------"
-puts "   |   |   "
+puts "   | X |   "
 puts "-----------"
 puts "   |   |   "
 end
